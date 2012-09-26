4d1632ae98411437349a8c664f586e71d52fe3d0
Eric Mill <konklone@gmail.com>
2012-09-26 17:10:19 -0400

    Better output

--- tasks/parse.py
+++ tasks/parse.py
@@ -1,8 +1,6 @@
+import os, json
 import utils
-from utils import log
-
 import uscode
-import json
 
 def run(options):
   title_number = options.get('title', None)
@@ -14,22 +12,32 @@ def run(options):
     return
 
   filename = utils.title_filename(title_number, year)
+  if not os.path.exists(filename):
+    utils.log("This title has not been downloaded.")
+
   title = uscode.title_for(filename)
 
   sections = title.sections()
 
+  count = 0
   for section in sections:
     section_number = section.enum()
     print "[%s USC %s] Parsing..." % (title_number, section_number)
+
+    # this could probably be black boxed a little further?
     bb = section.body_lines()
     xx = uscode.GPOLocatorParser(bb)
     qq = xx.parse()
     output = qq.json()
-    
+
     utils.write(
       json.dumps(output, sort_keys=True, indent=2), 
       uscode_output(year, title_number, section_number)
     )
+
+    count += 1
+
+  print "\nParsed %s sections of title %s." % (count, title_number)
 
 
 def uscode_output(year, title, section):
