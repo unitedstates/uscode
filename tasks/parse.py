dbf4c892b77a6e378522b18634f3036f427544da
Eric Mill <konklone@gmail.com>
2012-09-26 17:07:50 -0400

    Parse and output every section in a title

--- tasks/parse.py
+++ tasks/parse.py
@@ -2,10 +2,11 @@ import utils
 from utils import log
 
 import uscode
+import json
 
 def run(options):
   title_number = options.get('title', None)
-  section = options.get('section', None)
+  # section = options.get('section', None)
   year = options.get('year', 2011) # default to 2011 for now
 
   if not title_number:
@@ -15,4 +16,21 @@ def run(options):
   filename = utils.title_filename(title_number, year)
   title = uscode.title_for(filename)
 
-  print title
+  sections = title.sections()
+
+  for section in sections:
+    section_number = section.enum()
+    print "[%s USC %s] Parsing..." % (title_number, section_number)
+    bb = section.body_lines()
+    xx = uscode.GPOLocatorParser(bb)
+    qq = xx.parse()
+    output = qq.json()
+    
+    utils.write(
+      json.dumps(output, sort_keys=True, indent=2), 
+      uscode_output(year, title_number, section_number)
+    )
+
+
+def uscode_output(year, title, section):
+  return "%s/%s/%s/%s.json" % (utils.output_dir(), year, title, section)
